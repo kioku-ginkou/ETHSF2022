08055bf934b34016aedd30ff521529c2757c9fe2
Taro Ishihara <taro.ishihara.mail@gmail.com>
2022-11-06 10:40:24 -0800

    donedone

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,10 @@
 <template>
-  <div>This is home</div>
-  <router-link to="/about">click here!</router-link>
+  <router-link class="firstlink" to="/about">click here!</router-link>
+  <h3>so far, please please use private mode on your browser XD</h3>
+  <img src="@/assets/skale.png" />
 </template>
+<style>
+.firstlink {
+  font-size: 20px;
+}
+</style>
